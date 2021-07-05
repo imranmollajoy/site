eb18af08a1d61ad82244769d36605ddd36260aac
imranmollajoy <imranmollajoy@gmail.com>
2021-07-06 00:16:34 +0600

    upddate gitigmore

---
+++ assets/js/scrollmagic-anim.js
@@ -0,0 +1,10 @@
+// // init controller
+// var controller = new ScrollMagic.Controller();
+
+// // create a scene
+// new ScrollMagic.Scene({
+//     duration: 500, // the scene should last for a scroll distance of 100px
+//     offset: 50 // start this scene after scrolling for 50px
+// })
+//     .setPin('.explore-blog') // pins the element for the the scene's duration
+//     .addTo(controller); // assign the scene to the controller
